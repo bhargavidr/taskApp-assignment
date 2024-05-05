29bd57810d3e2f6f63a51647c773a2503ad25247
ShreyasDes <168353521+ShreyasDes@users.noreply.github.com>
2024-05-05 10:48:10 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from "./components/Home";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Account from "./components/Account"; 
 import Tasks from "./components/Tasks"
+import TaskDetails from "./components/TaskDetails"; // Import the TaskDetails component
 import { useAuth } from './context/AuthContext';
-
-
 
 function App() {
   const { user, dispatch } = useAuth();
@@ -22,23 +22,22 @@ function App() {
         </>
       ) : (
         <>
-       
           <Link to="/tasks">Tasks</Link> |
           <Link to="/account">Account</Link> |
           <Link to="/" onClick={() => {
             localStorage.removeItem('token');
             dispatch('LOGOUT');
           }}> Logout </Link> |
-         
         </>
       )}
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path='/tasks' element={<Tasks />} />
         <Route path="/login" element={<Login />} />
         <Route path="/account" element={<Account />} /> 
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/task/:taskId" element={<TaskDetails />} /> {/* Add this route for task details */}
       </Routes>
     </div>
   );
